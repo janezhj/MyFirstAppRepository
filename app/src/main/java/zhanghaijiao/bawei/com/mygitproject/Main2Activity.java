314aa70bf6b69083c5aad267d0d32469df655a97
jane <874059604@qq.com>
2018-04-13 18:15:03 +0800

    添加了textView组件

---
+++ app/src/main/java/zhanghaijiao/bawei/com/mygitproject/Main2Activity.java
@@ -0,0 +1,17 @@
+package zhanghaijiao.bawei.com.mygitproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView textView=findViewById(R.id.tv);
+
+
+    }
+}
