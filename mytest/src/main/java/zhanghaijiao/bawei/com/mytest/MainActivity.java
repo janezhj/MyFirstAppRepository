40a8bb9764a63a9216dfe5b3e979480f7e1f4859
jane <874059604@qq.com>
2018-04-17 16:35:22 +0800

    添加了textivew组件，第二次提交

--- mytest/src/main/java/zhanghaijiao/bawei/com/mytest/MainActivity.java
+++ mytest/src/main/java/zhanghaijiao/bawei/com/mytest/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView textView=findViewById(R.id.tv);
         textView.setText("这是测试。。。");
+
     }
 }
